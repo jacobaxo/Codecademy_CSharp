857222389953a196572d74c0324bf58caa1585a8
mays_jacobj <mays_jacobj@G-02-19>
2023-11-02 12:41:38 -0400

    Add project files.

---
+++ Codecademy/Logic and Conditionals.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int minLength = 7;
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string didgits = "012356789";
+            string specialChars = "!@#$%^&*()_-+=~`[];:,.<>";
+
+            Console.WriteLine("Please input a password: ");
+            string password = Console.ReadLine();
+
+            int score = 0;
+
+            if (score <= 0)
+            {
+                score++;
+            }
+            if (Tools.Contains(password, lowercase))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, uppercase))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, didgits))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, specialChars))
+            {
+                score++;
+            }
+            Console.WriteLine(score);
+            switch (score)
+            {
+                case 1:
+                    Console.WriteLine("Weak.");
+                    break;
+                case 2:
+                    Console.WriteLine("Medium.");
+                    break;
+                case 3:
+                    Console.WriteLine("Strong.");
+                    break;
+                case 4:
+                    Console.WriteLine("Extremly strong!");
+                    break;
+                case 5:
+                    Console.WriteLine("Extremely strong again!");
+                    break;
+                default:
+                    Console.WriteLine("you aint do it right");
+                    break;
+            }
+        }
+    }
+}
---
+++ Codecademy/Methods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+static void Main(string[] args)
+{
+    Console.WriteLine("What monument would you like to work with?( TAJ MAHAL or AL HARAM )");
+    string answer = Console.ReadLine();
+    answer = answer.ToUpper();
+
+    switch (answer)
+    {
+
+        case "TAJ MAHAL":
+            CalculateTotalCost(answer);
+            break;
+
+        case "AL HARAM":
+            CalculateTotalCost(answer);
+            break;
+
+        default:
+            Console.WriteLine("Error, please restart the program");
+            break;
+
+    }
+}
+static double CalculateTotalCost(string Building)
+{
+    switch (Building)
+    {
+        case "TAJ MAHAL":
+            double triArea = 24 * 24 * 2 * 4;
+            double totalArea = 90.5 * 90.5 - triArea;
+            double price = totalArea * 180;
+            Console.WriteLine($"{price} poses");
+            return price;
+
+
+        case "AL HARAM":
+            double triArea2 = 264 * 84 / 2;
+            double bigRectArea = 284 * 264 - triArea2;
+            double smallRectArea = 180 * 106;
+            double totalArea2 = bigRectArea + smallRectArea;
+            double price2 = totalArea2 * 180;
+            Console.WriteLine($"{price2} poses");
+            return price2;
+
+
+
+        default:
+            Console.WriteLine("error");
+            return 0;
+    }
+}
